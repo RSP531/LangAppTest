80058e2bcae7d22bb42a9296b7382498c84ed5de
Robert Peschke <robert.peschke@fiserv.com>
2021-08-26 11:03:34 -0400

    working

---
+++ app/src/main/java/com/example/langapptest/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.langapptest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageButton en = findViewById(R.id.btn_en);
+        ImageButton kr = findViewById(R.id.btn_ko);
+        ImageButton kh = findViewById(R.id.btn_kh);
+        LanguageManager lang = new LanguageManager(this);
+        en.setOnClickListener(view -> {
+            lang.updateResource("en");
+            recreate();
+        });
+        kr.setOnClickListener(view -> {
+            lang.updateResource("ko");
+            recreate();
+        });
+        kh.setOnClickListener(view -> {
+            lang.updateResource("km");
+            recreate();
+
+        });
+
+        findViewById(R.id.btn_go).setOnClickListener(view ->{
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        });
+
+    }
+}
